e68c0ff7a895cd96f829196fef74f4a0d768ef16
sunxu <ntsunxu@163.com>
2018-12-28 12:54:59 +0800

    fix display error in prod

--- src/components/leftside/LeftSide.vue
+++ src/components/leftside/LeftSide.vue
@@ -1,41 +1,41 @@
 <template>
-  <v-navigation-drawer
-    :v-model="mobile && show"
-    :mini-variant="!mobile && !show"
-    :mobile-break-point="-1"
-    :class="[$style.drawer, mobile ? show ? $style.maxWidth : $style.hide : undefined]"
-    :style="{minWidth: show ? '300px' : '80px'}"
-  >
-    <!-- todo: 添加用户操作 -->
-    <v-list class="pa-1">
-      <v-list-tile avatar tag="div">
-        <v-list-tile-avatar>
-          <img :src="user == null ? Logo : user.avatar">
-        </v-list-tile-avatar>
-        <v-list-tile-content>
-          <v-list-tile-title>{{user == null ? '' : user.name}}</v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list>
+    <v-navigation-drawer
+        :v-model="mobile && show"
+        :mini-variant="!mobile && !show"
+        :mobile-break-point="-1"
+        :class="[$style.drawer, mobile ? show ? $style.maxWidth : $style.hide : undefined]"
+        :style="{minWidth: show ? '300px' : '80px'}"
+    >
+        <!-- todo: 添加用户操作 -->
+        <v-list class="pa-1">
+            <v-list-tile avatar tag="div">
+                <v-list-tile-avatar>
+                    <img :src="user == null ? Logo : user.avatar">
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                    <v-list-tile-title>{{user == null ? '' : user.name}}</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
 
-    <v-divider light></v-divider>
-    <v-list class="pt-0" ref="categoryList" :class="$style.categoryList">
-      <transition-group name="list">
-        <category-list-item
-          v-for="(item, i) in items"
-          :key="item.title"
-          :mobile="mobile"
-          :show="show"
-          :category="item"
-          :items="items"
-          :index="i"
-        />
-      </transition-group>
-    </v-list>
+        <v-divider light></v-divider>
+        <v-list class="pt-0" ref="categoryList" :class="$style.categoryList">
+            <transition-group name="list">
+                <category-list-item
+                    v-for="(item, i) in items"
+                    :key="item.title"
+                    :mobile="mobile"
+                    :show="show"
+                    :category="item"
+                    :items="items"
+                    :index="i"
+                />
+            </transition-group>
+        </v-list>
 
-    <v-divider light></v-divider>
-    <add-category-item v-show="mobile || show" :items="items" @addNewCategory="addNewCategory"/>
-  </v-navigation-drawer>
+        <v-divider light></v-divider>
+        <add-category-item v-show="mobile || show" :items="items" @addNewCategory="addNewCategory"/>
+    </v-navigation-drawer>
 </template>
 
 <script>
@@ -48,7 +48,7 @@ export default {
     show: Boolean,
     mobile: Boolean
   },
-  data () {
+  data() {
     let user = this.$store.getters.user
     let items = this.$store.getters.categories
     return {
@@ -62,7 +62,7 @@ export default {
     AddCategoryItem
   },
   methods: {
-    addNewCategory (newName) {
+    addNewCategory(newName) {
       this.$store.commit('addCategory', newName)
     }
   }
@@ -71,7 +71,7 @@ export default {
 
 <style lang="scss" module>
 .drawer {
-  display: flex;
+  display: flex !important;
   flex-direction: column;
   justify-content: space-between;
 
