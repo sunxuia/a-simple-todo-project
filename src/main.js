f967abc031ef6173c534dbc6ab6791c7e2226348
sunxu <ntsunxu@163.com>
2018-12-28 13:56:23 +0800

    add roboto font locally

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'typeface-roboto/index.css'
 import VueDateTime from 'vue-datetime'
 
 import './common/date-utils'
